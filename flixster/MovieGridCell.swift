d081101065235704f38c57dacd0f0c605d61b813
Diana Garcia <Diagarcia@csumb.edu>
2021-02-15 15:24:55 -0800

    assignment 02 - added a gallery view for superhero movies and view more section for the now playing tab.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Diana Garcia Davalos on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ flixster/MovieGridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  flixster
+//
+//  Created by Diana Garcia Davalos on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+}
